2d066c166f9062ed97922a08c838131af178fa78
Curt Bryan <bryan.curt@univ-jfc.fr>
2020-03-19 18:46:26 +0100

    grosse avancee mais ça marche pas

--- src/secu/PB.java
+++ src/secu/PB.java
@@ -1,5 +1,10 @@
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -7,70 +12,98 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
-
-
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class PB {
-	public static void main(String[] args) {
-		InetAddress addr;
-		Socket client;
-		PrintWriter out;
-		BufferedReader in;
-		String input;
-		String userInput;
-		boolean doRun = true;
-		
-		
-		Scanner k = new Scanner(System.in);
-		try{
-			client = new Socket("localhost", 4444);
-			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			out = new PrintWriter(client.getOutputStream(), true);
-			
-			System.out.print("enter msg> ");
-			userInput = k.nextLine();
-			out.println(userInput);
-			out.flush();
-			System.out.println("done");
-			
-			if(userInput.compareToIgnoreCase("bye")==0)
-			{
-				System.out.println("shutting down");
-				doRun = false;
-			}else
-			{
-				while(doRun){
-					input = in.readLine();
-					while(input == null) input = in.readLine();
-					
-					System.out.println(input);
-					if(input.compareToIgnoreCase("bye")==0)
-					{
-						System.out.println("client shutting down from server request");
-						doRun = false;
-					}else
-					{
-						System.out.print("enter msg> ");
-						userInput = k.nextLine();
-						out.println(userInput);
-						out.flush();
-						if(userInput.compareToIgnoreCase("bye")==0)
-						{
-							System.out.println("shutting down");
-							doRun = false;
-						}
-						
-					}
-				}
-			}
-			client.close();
-			k.close();
-		}catch(UnknownHostException e){
-			e.printStackTrace();
-		}
-		catch(IOException ioe){
-			System.out.println(ioe.getMessage());
-		}
-	}
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, FileNotFoundException, IOException {
+        InetAddress addr;
+        Socket client;
+        PrintWriter out;
+        BufferedReader in;
+        String input;
+        String userInput;
+        boolean doRun = true;
+        
+        File f = new File("key.txt");
+        FileReader fr = new FileReader(f.getAbsoluteFile());
+        BufferedReader br = new BufferedReader(fr);
+        String k2r = br.readLine();
+       
+        br.close();
+        fr.close();
+
+        Scanner k = new Scanner(System.in);
+
+        try {
+            client = new Socket("localhost", 4444);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+
+            System.out.print("enter msg> ");
+            userInput = k.nextLine();
+            out.println(userInput);
+            out.flush();
+            System.out.println("done");
+
+            SecretKeySpec specification = new SecretKeySpec(userInput.getBytes(), "AES");
+            byte[] bytes = null;
+            
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(128);
+            SecretKey key = generator.generateKey();
+            String k2w;
+            byte[] encoded = key.getEncoded();
+            k2w = Base64.encode(encoded);
+            String key2 = k2w;
+            
+            Cipher chiffreur = Cipher.getInstance("AES");
+            chiffreur.init(Cipher.ENCRYPT_MODE, specification);
+            bytes = chiffreur.doFinal(key2.getBytes());
+            System.out.println(new String(bytes));
+
+            if (userInput.compareToIgnoreCase("bye") == 0) {
+                System.out.println("shutting down");
+                doRun = false;
+            } else {
+                while (doRun) {
+                    input = in.readLine();
+                    while (input == null) {
+                        input = in.readLine();
+                    }
+
+                    System.out.println(input);
+                    if (input.compareToIgnoreCase("bye") == 0) {
+                        System.out.println("client shutting down from server request");
+                        doRun = false;
+                    } else {
+                        System.out.print("enter msg> ");
+                        userInput = k.nextLine();
+                        out.println(userInput);
+                        out.flush();
+                        if (userInput.compareToIgnoreCase("bye") == 0) {
+                            System.out.println("shutting down");
+                            doRun = false;
+                        }
+
+                    }
+                }
+            }
+            client.close();
+            k.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+    }
 }
--- src/secu/PC.java
+++ src/secu/PC.java
@@ -1,52 +1,68 @@
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 //import java.net.UnknownHostException;
 
 public class PC {
+	static  ServerSocket server;
+	static int clientID = 0;
+	
+	public static void main(String ard[]){
+		
+		try{
+                    File f = new File("key.txt");
+            System.out.println("création d'un fichier et de la clé");
+            //on génère la clé
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(128);
+            SecretKey key = generator.generateKey();
+            //on va désormais "transformer" la clé en string pour 
+            //l'écrire dans un fichier
+            String k2w;
+            byte[] encoded = key.getEncoded();
+            k2w = Base64.encode(encoded);
+            FileWriter fw = new FileWriter(f.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(k2w);
+            bw.close();
+            fw.close();
+            System.out.println("écriture réalisée de la clé : " + k2w);
+			server = new ServerSocket(4444, 5);//5 connexions clientes au plus
+			go();
+		}catch(Exception e){}
+	}
+	
+	
+	public static void go() {	
+		
+		
+		try{			
+			Thread t = new Thread(new Runnable(){
+				public void run(){
+					while(true)//
+					{
+						try{
+							Socket client = server.accept();
+							// Faire tourner le socket qui s'occupe de ce client dans son propre thread et revenir en attente de la prochaine connexion
+							// Le chat avec l'entit� connect�e est encapsul� par une instance de ChatServer
+							Thread tAccueil = new Thread(new ChatServer(client, clientID));
+							tAccueil.start();
+							clientID++;
+						}catch(Exception e){}
+					}
+				}
+			});
+			t.start();
 
-    static ServerSocket server;
-    static int clientID = 0;
-
-    public static void main(String ard[]) {
-
-        try {
-            Cipher chiffreur = Cipher.getInstance("AES");
-            //chiffreur.init(Cipher.ENCRYPT_MODE, specification);
-           // bytes = chiffreur.doFinal(args[1].getBytes());
-            server = new ServerSocket(4444, 5);//5 connexions clientes au plus
-            go();
-        } catch (Exception e) {
-        }
-    }
-
-    public static void go() {
-
-        try {
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    while (true)//
-                    {
-                        try {
-                            Socket client = server.accept();
-                            // Faire tourner le socket qui s'occupe de ce client dans son propre thread et revenir en attente de la prochaine connexion
-                            // Le chat avec l'entit� connect�e est encapsul� par une instance de ChatServer
-                            Thread tAccueil = new Thread(new ChatServer(client, clientID));
-                            tAccueil.start();
-                            clientID++;
-                        } catch (Exception e) {
-                        }
-                    }
-                }
-            });
-            t.start();
-
-        } catch (Exception i) {
-            System.out.println("Impossible d'�couter sur le port 4444: serait-il occup�?");
-            i.printStackTrace();
-        }
-    }
+		}
+		catch(Exception i){
+			System.out.println("Impossible d'�couter sur le port 4444: serait-il occup�?");		
+			i.printStackTrace();
+		}
+	}
 }
